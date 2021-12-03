032c7ad4f9f6521088350decada7a12097baf90c
Nick McCollum <nmccollum81@gmail.com>
2021-12-02 18:01:49 -0800

    Add TextInputMono

---
+++ components/TextInputMono.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { TextInput } from "react-native-paper";
+
+const TextInputMono = ({ ...props }: any) => {
+  return <TextInput {...props} />;
+};
+
+export default TextInputMono;
---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../components/TextInputMono.stories");
+}
+
+const stories = ["../components/TextInputMono.stories"];
+
+module.exports = {
+  loadStories,
+  stories,
+};
